837881afd3986cfb8ca0d94fee99d22d7f382bd7
SanaaMohammed <sanaaelhaproty@gmail.com>
2024-03-12 12:20:02 +0200

    Operator ovrloading on objects

---
+++ Operator Oveloading/Money.cs
@@ -0,0 +1,52 @@
+﻿namespace Csharp
+{
+    class Money
+    {
+        private decimal amount;
+
+        public decimal Amount => amount;
+
+        public Money(decimal amount)
+        {
+            this.amount = amount;
+        }
+
+        // applay Operator Overloading in +
+
+        public static Money operator +(Money a, Money b)
+        {
+            var value = a.Amount + b.Amount;
+            return new Money(value);
+        }
+
+
+        // applay Operator Overloading in -
+
+        public static Money operator -(Money a, Money b)
+        {
+            var value = a.Amount - b.Amount;
+            return new Money(value);
+        }
+
+        // applay Operator Overloading in *
+
+        public static Money operator *(Money a, Money b)
+        {
+            var value = a.Amount * b.Amount;
+            return new Money(value);
+        }
+        // applay Operator Overloading in /
+
+        public static Money operator /(Money a, Money b)
+        {
+            var value = a.Amount / b.Amount;
+            return new Money(Math.Round(value,2));
+        }
+
+
+    }
+
+
+
+
+}
---
+++ Operator Oveloading/OperatorOverloading.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+namespace Csharp
+{
+    class OperatorOverloading
+    {
+
+        static void Main()
+        {
+            Money money1 = new Money(100);
+            Money money2 = new Money(20);
+
+            // Operator Overloading
+            Money money3 = money1 + money2;
+            Money money4 = money1 - money2;
+            Money money5 = money1 * money2;
+            Money money6 = money1 / money2;
+
+            Console.WriteLine($"Money after applaing Operator Overloading in +  = {money3.Amount}");
+            Console.WriteLine($"Money after applaing Operator Overloading in -  = {money4.Amount}");
+            Console.WriteLine($"Money after applaing Operator Overloading in *  = {money5.Amount}");
+            Console.WriteLine($"Money after applaing Operator Overloading in /  = {money6.Amount}");
+
+
+        }
+
+    }
+
+
+}

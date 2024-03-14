fac433205de27829d7dfbc1ec3a31f7b854e7738
SanaaMohammed <sanaaelhaproty@gmail.com>
2024-03-14 14:01:34 +0200

    Extension method update

---
+++ Extension Method/ExtensionMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+namespace Csharp
+{
+    class ExtensionMethod
+    {
+
+        static void Main()
+        {
+            int x = 12345;
+            int y = x.Mirror();
+
+            string str = "123";
+            str = str.Mirror2();
+
+            Console.WriteLine(y);
+            Console.WriteLine(str);
+        }
+        
+
+    }
+
+   
+
+}
---
+++ Extension Method/Extension_Method_Impelemetation.cs
@@ -0,0 +1,23 @@
+﻿namespace Csharp
+{
+    static class Extension_Method_Impelemetation
+    {
+         public static int Mirror(this int i)
+        {
+            var temp=i.ToString().ToCharArray();
+            temp.Reverse();
+
+            return int.Parse(new string(temp));
+        }
+
+        public static string Mirror2(this string i)
+        {
+            var temp = Convert.ToInt32(i);
+            var str = (temp * 10).ToString();
+            return str;
+        }
+    }
+
+   
+
+}
